117d697f9f04a5f65a970bb53a84302a07481ca1
CPAREDES27 <cparedes@xternal.biz>
2022-09-23 09:32:18 -0500

    environment_prd

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  urlService: "https://localhost:7275/api/",
+  urlService: "https://back.envirored.com/api/",
   DANGEROUSLY_DISABLE_HOST_CHECK: true,
   disableHostCheck: true
 };
